4685b9713b632f18a366dfb431093e41a968f149
Lukas Sanner <lukas.sanner@lgln.niedersachsen.de>
2025-01-21 15:43:52 +0100

    options init

---
+++ src/some/options/__init__.py
@@ -0,0 +1,12 @@
+from some.options.dag import LazyDAGSome, LazyDAGSomeFnType
+from some.options.lazy import LazySome, LazySomeFnType
+from some.options.some import Some, SomeFnType
+
+__all__ = [
+    "LazySome",
+    "LazySomeFnType",
+    "LazyDAGSome",
+    "LazyDAGSomeFnType",
+    "Some",
+    "SomeFnType",
+]
