bbfee1b5cc950228adc8f18b5e555be6f7930456
Lukas Sanner <lukas.sanner@lgln.niedersachsen.de>
2025-01-21 15:43:27 +0100

    init

---
+++ src/some/__init__.py
@@ -0,0 +1,11 @@
+from some.options import LazyDAGSome, LazySome, Some
+from some.types import Option
+from some.wrappers import some
+
+__all__ = [
+    "Some",
+    "LazyDAGSome",
+    "LazySome",
+    "Option",
+    "some",
+]
